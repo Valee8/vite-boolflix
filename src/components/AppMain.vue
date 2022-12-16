4fcbf67a6694f3221dcdcd38659e9ad5934084b0
Vale <oirelav95@gmail.com>
2022-12-16 11:27:37 +0100

    creo componente AppMain

---
+++ src/components/AppMain.vue
@@ -0,0 +1,23 @@
+<script>
+
+export default {
+    name: "AppMain",
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<template>
+
+    <h1>
+        AppMain
+    </h1>
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+</style>
