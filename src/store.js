4d3d725ca2d8a106e50bd86abe7cc7b3403c982d
Vale <oirelav95@gmail.com>
2022-12-16 14:10:34 +0100

    aggiungo searchText che contiene valore dell'input

--- src/store.js
+++ src/store.js
@@ -3,4 +3,5 @@ import { reactive } from 'vue'
 export const store = reactive({
     cardsList: [],
     apiURL: "https://api.themoviedb.org/3/search/movie?api_key=a13e0fd79cfa1dd9f0f1686cc7c31b44&query=one+piece&language=it-IT",
+    searchText: '',
 });
