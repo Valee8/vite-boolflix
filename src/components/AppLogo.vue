33abf3863a65089903b7260c3fff5e7a8fa7c718
Vale <oirelav95@gmail.com>
2022-12-16 11:50:48 +0100

    creo componente AppLogo

---
+++ src/components/AppLogo.vue
@@ -0,0 +1,20 @@
+<script>
+
+export default {
+    name: "AppLogo",
+    data() {
+        return {
+        }
+    }
+}
+
+</script>
+
+<template>
+
+
+</template>
+
+<style lang="scss" scoped>
+@use '../styles/general.scss' as *;
+</style>
