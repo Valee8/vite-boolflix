f0d905aca07b6b8443c61fec54e4ed5a1735bfc9
Vale <oirelav95@gmail.com>
2022-12-16 13:00:34 +0100

    importo store in App

--- src/App.vue
+++ src/App.vue
@@ -2,11 +2,19 @@
 import AppMain from './components/AppMain.vue'
 import AppHeader from './components/AppHeader.vue'
 
+import { store } from './store.js';
+
+
 export default {
   name: "App",
   components: {
     AppMain,
     AppHeader
+  },
+  data() {
+    return {
+      store,
+    }
   }
 }
 </script>
@@ -17,10 +25,10 @@ export default {
 
   <main>
 
-
     <AppMain />
 
   </main>
+
 </template>
 
 <style lang="scss">
