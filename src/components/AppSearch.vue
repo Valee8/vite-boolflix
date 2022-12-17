bc21f3e377f6ae1fb47cc9b403612f6d092d0b2f
Vale <oirelav95@gmail.com>
2022-12-17 14:11:47 +0100

    aggiungo stili a button e input

--- src/components/AppSearch.vue
+++ src/components/AppSearch.vue
@@ -18,8 +18,8 @@ export default {
 
     <div class="container">
         <form>
-            <input type="search" placeholder="Cerca" v-model="store.searchText">
-            <button type="submit" @click.prevent="$emit('search')">Invia</button>
+            <input type="search" placeholder="Cerca" v-model="store.searchText" class="input-search">
+            <button type="submit" @click.prevent="$emit('search')" class="button-search">Invia</button>
         </form>
     </div>
 
@@ -28,4 +28,19 @@ export default {
 
 <style lang="scss" scoped>
 @use '../styles/general.scss' as *;
+
+@use '../styles/partials/mixins' as *;
+
+form>* {
+    background-color: #ccc;
+    padding: 5px;
+    border-radius: 3px;
+    border: 0;
+    color: #000;
+}
+
+.button-search {
+    margin-left: 5px;
+    cursor: pointer;
+}
 </style>
