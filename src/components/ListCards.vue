f455fed353350ca91d91499f63eb321a0fcdd07a
Vale <oirelav95@gmail.com>
2022-12-17 16:18:00 +0100

    nascondo card che non hanno copertina

--- src/components/ListCards.vue
+++ src/components/ListCards.vue
@@ -21,7 +21,7 @@ export default {
 
 <template>
 
-    <div v-for="card in store.cardsList" :key="card.id" :class="card.poster_path ? 'card' : 'card-no-path'">
+    <div v-for="card in store.cardsList" :key="card.id" :class="card.poster_path ? 'card' : 'card no-path'">
         <Card :info="card" />
     </div>
 
@@ -35,7 +35,7 @@ export default {
     margin: 10px;
 }
 
-.card-no-path {
+.card.no-path {
     display: none;
 }
 </style>
